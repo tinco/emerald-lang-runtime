c6b9364b86264940f1aee65ac2822f755fd8436f
Tinco Andringa <mail@tinco.nl>
2024-12-16 10:27:20 +0100

    start working on making cranelift jit

--- codegen/src/main.rs
+++ codegen/src/main.rs
@@ -1,7 +1,7 @@
 #![feature(rustc_private)]
 
 extern crate rustc_codegen_cranelift;
-extern crate rustc_codegen_llvm;
+extern crate rustc_codegen_ssa;
 extern crate rustc_driver;
 extern crate rustc_error_codes;
 extern crate rustc_errors;
@@ -18,6 +18,8 @@ use rustc_errors::registry;
 use rustc_hash::FxHashMap;
 use rustc_session::config;
 use rustc_interface::{Linker, passes};
+use rustc_codegen_cranelift::{CraneliftCodegenBackend, BackendConfig, CodegenMode};
+use rustc_codegen_ssa::traits::CodegenBackend;
 
 fn main() {
     let out = process::Command::new("rustc")
@@ -26,6 +28,7 @@ fn main() {
         .output()
         .unwrap();
     let sysroot = str::from_utf8(&out.stdout).unwrap().trim();
+    println!("sysroot: {}", sysroot);
 
     let working_dir = std::env::current_dir().expect("Current directory is invalid");
     let output_types = config::OutputTypes::new(&[(config::OutputType::Exe, None)]);
@@ -35,17 +38,25 @@ fn main() {
         name: rustc_span::FileName::Custom("main.rs".into()),
         input: r#"
 static HELLO: &str = "Hello, world!";
-fn main() {
+pub fn main() {
 println!("{HELLO}");
 }
 "#
         .into(),
     };
 
+    let backend = CraneliftCodegenBackend {
+        config: Some(BackendConfig {
+            codegen_mode: CodegenMode::Jit,
+            jit_args: vec![],
+        }),
+    };
+    backend.print_version();
+
     let mut config = rustc_interface::Config {
         opts: config::Options {
-            // maybe_sysroot: Some(path::PathBuf::from(sysroot)),
-            maybe_sysroot: None,
+            maybe_sysroot: Some(path::PathBuf::from(sysroot)),
+            // maybe_sysroot: None,
             output_types,
             ..config::Options::default()
         },
@@ -53,7 +64,8 @@ println!("{HELLO}");
         crate_check_cfg: Vec::new(),
         input: input,
         output_dir: None,  // Option<PathBuf>
-        output_file: Some(config::OutFileName::Real("./out.o".into())),
+        // output_file: Some(config::OutFileName::Real("./out.o".into())),
+        output_file: None,
         ice_file: None,
         file_loader: None,
         locale_resources: rustc_driver::DEFAULT_LOCALE_RESOURCES.to_vec(),
@@ -64,9 +76,7 @@ println!("{HELLO}");
         override_queries: None,
         make_codegen_backend: Some(Box::new(|_opts| {
             // rustc_codegen_llvm::LlvmCodegenBackend::new()
-            Box::new(rustc_codegen_cranelift::CraneliftCodegenBackend {
-                config: None,
-            })
+            Box::new(backend)
         })),
         registry: registry::Registry::new(rustc_errors::codes::DIAGNOSTICS),
         using_internal_features: Arc::default(),
